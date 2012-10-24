da69fb3e0d2be1dc03da62de83e4a42b1494b286
bhaviksingh <bhaviksingh@gmail.com>
2012-10-23 19:24:47 -0700

    new skeleton for rip

--- p2 routing/rip_router.py
+++ p2 routing/rip_router.py
@@ -16,53 +16,44 @@ class RIPRouter (Entity):
 
     def handle_rx (self, packet, port):
 
-        #returns numerical value of the minimum cost to destination dest
-        def min_cost(dest):
-        	destTable = self.routingTable[dest]
-        	return destTable[min(destTable, key=destTable.get)]
+    	def update_min_costs():
+    		preChange = self.minCosts
+    		#TODO!
+    		return preChange == self.minCosts
 
-        #discovery packet
-        if hasattr(packet, 'is_link_up'):
-        	print "Discovery packet: ", packet, " at: ", self, " on port ", port
-        	if packet.is_link_up:
-        		# so i know how to get to my neighbours
-        		self.ports[packet.src] = port
-        		#there may need to be more error checks for this line
-        		self.routingTable[packet.src] = {packet.src : 1}
-	    		updatePacket = RoutingUpdate()
-	    		updatePacket.add_destination(packet.src, self.routingTable[packet.src][packet.src])
-	    		self.send(updatePacket, port, flood=True)
-	    		print "Sending updatepacket ", updatePacket.str_routing_table()
-        	else:
-        		print "Down link"
-        		
+    	def gen_update(to):
+    		updatePacket = RoutingUpdate()
+    		for dest, throughcost in minCosts: #for each dest
+    			if dest != to: # if its not where im sending
+    				updatePacket.add_destination(dest, throughcost[1])
+    		return updatePacket
 
+    	
+    	#main body	
+    	print self, " got ", packet, " at ", port
 
-        #routingupdate packet
-        elif hasattr(packet, 'paths'):
-        	print "Routingupdate packet: ", packet, " at: ", self, " on port ", port
-        	destinations = packet.all_dests();
-        	updatePacket = RoutingUpdate()
-        	for dest in destinations:
-        		throughSrc = min_cost(packet.src) + packet.get_distance(dest)
-        		#if i dont have any info on how to get to dest, add info for it, going through packet.src
-        		if not self.routingTable.has_key(dest):
-        			self.routingTable[dest] = { packet.src : throughSrc}
-        			updatePacket.add_destination(dest, throughSrc)
-        		#if i have info on how to get to it, only update things if the path through packet.src is shorter
-        		elif  throughSrc < min_cost(dest):
-        			self.routingTable[dest][packet.src] =  throughSrc
-	        		updatePacket.add_destination(dest, throughSrc)
-	        #send out an update packet if something is changed
-	        if (len(updatePacket.all_dests()) >0):
-        		self.send(updatePacket, port, flood = True)
-        		print "Sending updatepacket ", updatePacket.str_routing_table()
-        
-        #if its a data packet, just send it to the router who i know will give me the shortest distance (decrement hops also?)
-    	else:
-        	print "Data packet: ", packet, " at: ", self, " on port ", port
-	        destination = min(self.routingTable[packet.dst], key=self.routingTable[packet.dst].get)
-   	    	self.send(packet, self.ports[destination])
+    	if has_attr(packet, 'is_link_up'):
+    		self.ports[packet.src] = port #how to get to my neighbours
+    		if packet.is_link_up:
+    			routingTable[packet.src] =  {packet.src: 1}
+    		else:
+    			if not routingTable.has_key(packet.src): #todo: remove
+    				print "OMGWTFF"
+    			routingTable.pop(packet.src) 
+       
+    	elif has_attr(packet, 'paths'):
+    		for dest in packet.all_dests():
+    			throughSrc = minCosts[packet.src][1] + packet.get_distance(dest)
+    			if not self.routingTable[packet.src].has_key(dest): #no info for dest through src
+    				self.routingTable[packet.src] = {dest: throughSrc}
+    			elif throughSrc < routingTable[packet.src][dest]: #cost to dest through src has changed
+    				self.routingTable[packet.src][dest] = throughSrc
 
-        print  self, " TABLE is ", self.routingTable
-        print ""
+    	else: 
+    		send(packet, self.ports[minCosts[packet.dst][0]]) #its a data packet, send it through the least cost port
+
+    	if update_min_costs(): #regenerate the mincost table, returns true if its changed
+    		for neighbour,port in self.ports: #each of my neighbours
+    			updatePacket = gen_update(neighbour)
+    			if len(updatePacket) > 0
+    				send(updatePacket, port) #send an update
